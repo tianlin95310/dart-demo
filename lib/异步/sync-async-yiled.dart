28069ea5c7f525a826d2c843c3f4ceb9853b3a32
tianlin <li.tian.mc@qq.com>
2023-08-01 15:36:00 +0800

    update content

--- lib/异步/sync-async-yiled.dart
+++ lib/异步/sync-async-yiled.dart
@@ -1,8 +1,11 @@
 void main() {
-  print(funa()..cast().forEach(print));
-  print(funb()..forEach(print));
-  print(func()..then((value) => null));
-  print(fund()..forEach(print));
+  Stream<int> a = funa();
+  print(a.forEach(print));
+  // print(funb()..forEach(print));
+
+  Future<int> c = func();
+  print(c..then(print));
+  // print(fund()..forEach(print));
 }
 
 // 返回同步流
@@ -11,24 +14,28 @@ Stream<int> funa() async* {
   yield* Stream<int>.fromIterable([2 ,3]);
   yield 4;
 }
-// unsupported sync
-// Iterable<List> funb() sync {
+
+/// unsupported sync
+// Iterable<List> fune() sync {
 //   yield [];
 // }
-//返回迭代器
+
+//返回迭代类
 Iterable<int> funb() sync* {
   List<int> data1 = [1, 2, 3];
   yield* [4, 5];
   yield* data1.skip(2);
   yield 6;
 }
-//返回异步
-Future func() async {
-  return 123;
+
+//返回异步Future
+Future<int> func() async {
+  return 333;
 }
-//返回迭代器
+//返回迭代类
 Iterable<List> fund() sync* {
   yield [1];
   yield* [[2], [3]];
 }
+
 
